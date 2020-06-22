b8b40025745abc9043ae68aae12455b8bcaddb76
Artem <aaleksanyan@umass.edu>
2020-06-21 23:41:27 -0400

    added info about what goal is

--- featureExtract.py
+++ featureExtract.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 # Takes in a np array, windowSize, and windowShift to return a feature matrix where every line is a window.
-def createFeatureMatrix(arr, windowSize, windowShift):
+# For EDA, we are just taking the last column and calculating features for that per window. 
+# This makes our feature matrix size (n, 6)
+# For Face Features, we are taking 5 time domain features for each of the face features.
+# There are 32 facial feature columns, so output of matrix size (n, 32*5) 
+def createFeatureMatrix(arr, windowSize, windowShift, dataType="eda"):
+    
     pass
